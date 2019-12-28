437b6fa80d7bbe15e35e62bb72e7cce1850203a0
Ray Haleblian <ray@haleblian.com>
2019-12-27 16:23:08 -0800

    Proclaim the install location.

--- configure.bat
+++ configure.bat
@@ -5,6 +5,7 @@ rem oved: set VERBOSE=1
 
 "C:\Program Files\CMake\bin\cmake.exe" ^
 -G "Visual Studio 14 2015" -A x64 ^
+-DCMAKE_INSTALL_PREFIX "C:\Users\ray\Applications\USD-19.07"
 -DEMBREE_TUTORIALS=OFF ^
 -DJPEG_INCLUDE_DIR="%PXR_ROOT%\include" ^
 -DJPEG_LIBRARY="%PXR_ROOT%\lib" ^
